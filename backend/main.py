93bb7a5f79d4911ae2b54108cdadacbb3f8eb8a5
alxwong1991 <alxwong1991@gmail.com>
2025-03-19 21:54:38 +0800

    modular routes

--- backend/main.py
+++ backend/main.py
@@ -3,13 +3,10 @@ from fastapi import FastAPI, Depends
 from sqlmodel import Session
 from sqlalchemy import text
 from fastapi.middleware.cors import CORSMiddleware
-from routes.video import video_router
-from routes.websocket import websocket_router
+from routes import router
 from dotenv import load_dotenv
 from database import get_session
 from contextlib import asynccontextmanager
-from routes.auth import auth_router
-from routes.exercise_log import exercise_router
 
 
 load_dotenv(override=True)
@@ -35,10 +32,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(video_router)
-app.include_router(websocket_router)
-app.include_router(auth_router, prefix="/auth")
-app.include_router(exercise_router, prefix="/exercise", tags=["Exercise"])
+app.include_router(router)
 
 @app.get("/")
 def home():
